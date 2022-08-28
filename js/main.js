2b57cd058e020123ec5cdb73c17fb37273006135
jaicychoi <jaicychoi@gamil.com>
2022-08-28 14:05:41 +0900

    map css modified

--- js/main.js
+++ js/main.js
@@ -281,19 +281,20 @@ close_btn_wrap.addEventListener('click', () => {
 });
 
 //view map controll
-var container = document.getElementById('map');
-var options = {
-		center: new kakao.maps.LatLng(33.450701, 126.570667),
-		level: 3
-	};
+let container = document.querySelector('.map');
+let options = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    level: 3
+};
 
-var map = new kakao.maps.Map(container, options);
+let map = new kakao.maps.Map(container, options);
+let map_close = document.querySelector('.map_close');
 
 address.addEventListener('click', () => {
     body.classList.add('srolllock')
     view_map.classList.add('show');
 });
-view_map.addEventListener('click', () => {
+map_close.addEventListener('click', () => {
     body.classList.remove('srolllock')
     view_map.classList.remove('show');
 });
